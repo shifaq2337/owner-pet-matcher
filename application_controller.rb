aa383b165bdc9dd91f9e092f8d84acad5279bbe5
Kellyx1636 <kellyx1636@hstat.org>
2018-03-16 05:45:03 +0000

    Changed our idea of our end goal and mainly re-adjusted all code.

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,6 @@
 require 'dotenv/load'
 require 'bundler'
+require 'json'
 Bundler.require
 
 require_relative 'models/model.rb'
@@ -9,9 +10,20 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+<<<<<<< HEAD
   
   post '/result' do
     erb.index
     # puts params 
+=======
+  post '/result' do
+    puts params
+    @user_pref_personality = params[:personality]
+    @pets_matched = Pets.new(@user_pref_personality)
+    @pets_matched.get_pet_files
+
+    
+    erb :result
+>>>>>>> 34e3b9863883a13bbdf94121a9547dd962f2f198
   end
 end
--- models/model.rb
+++ models/model.rb
@@ -2,52 +2,37 @@ require 'net/http'
 require 'json'
 require 'pp'
 
-#
- 
-uri = URI(url)
-response = Net::HTTP.get(uri)
-result = JSON.parse(response)
-pets_info = result["petfinder"]["pets"]["pet"]
-# pp pets_info
-pets_info_copy = Array.new(pets_info)
-puts pets_info_copy[1]["description"]["$t"]
-# puts pets_info_copy[1]["description"]["$t"].include?("personality")
 
-# if pets_info_copy[1]["description"]["$t"].include?'playful'
-#     puts "I FOUND YOUR PET!"
-# else
-#     puts "Sorry, a pet cannot be found based the description that you gave just now."
-# end
-
-#loop does not work b/c info inside description becomes blank
-# i = 0
-# n = 25
-# until i > n do
-#     puts result["petfinder"]["pets"]["pet"]["description"]#[i]
-#     i+=1
-# end
-
-
-=begin
-def get_pet_file
-  
-end
-#puts get_pet_file
-
-class Pet 
-    attr_reader :animal_type, :zip_code 
-    def initialize(animal_type, zip_code)
-        @animal_type = animal_type
-        @zip_code = zip_code
-    end
+class Pets 
+   attr_reader :personality 
     
-    def get_pet_file
-
+   def initialize(personality) 
+        @personality = personality
+   end
+    def get_pet_files
+        url = ''
         uri = URI(url)
         response = Net::HTTP.get(uri)
         result = JSON.parse(response)
-        result["petfinder"]["pet"]["description"]["$t"]
+        pets_info = result["petfinder"]["pets"]["pet"]
+        pets_info_copy = Array.new(pets_info)
+            i = 1
+            n = 25
+            until i > n do
+                pet_description = pets_info_copy[i]["description"]["$t"]
+                word_match_info = pets_info_copy[i]["description"]["$t"].include?"#{@personality}"
+                pet_name = pets_info_copy[i]["name"]["$t"]
+                pet_pic = pets_info_copy[i]["media"]["photos"]["photo"][3]["$t"]
+                descriptions = {"#{word_match_info}"=> ["#{pet_name}","#{pet_pic}","#{pet_description}"]}
+            #descriptions.keys
+                puts descriptions.select{|k,v| k == "true"}
+                i+=1
+            end
     end
 end
-=end
+
+#TESTING  
+a = Pets.new("dog")
+puts a.get_pet_files
+
 
