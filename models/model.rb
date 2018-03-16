fea834a2579489830e76695d47678eb130334aef
shifaq2337 <shifaq2337@hstat.org>
2018-03-16 14:04:45 +0000

    update other files w/new code

--- models/model.rb
+++ models/model.rb
@@ -15,23 +15,23 @@ class Pets
         result = JSON.parse(response)
         pets_info = result["petfinder"]["pets"]["pet"]
         pets_info_copy = Array.new(pets_info)
-            i = 1
-            n = 25
-            until i > n do
-                pet_description = pets_info_copy[i]["description"]["$t"]
-                word_match_info = pets_info_copy[i]["description"]["$t"].include?"#{@personality}"
-                pet_name = pets_info_copy[i]["name"]["$t"]
-                pet_pic = pets_info_copy[i]["media"]["photos"]["photo"][3]["$t"]
-                descriptions = {"#{word_match_info}"=> ["#{pet_name}","#{pet_pic}","#{pet_description}"]}
+        i = 1
+        n = 25
+        until i > n do
+            pet_description = pets_info_copy[i]["description"]["$t"]
+            word_match_info = pets_info_copy[i]["description"]["$t"].include?"#{@personality}"
+            pet_name = pets_info_copy[i]["name"]["$t"]
+            pet_pic = pets_info_copy[i]["media"]["photos"]["photo"][3]["$t"]
+            descriptions = {"#{word_match_info}"=> ["#{pet_name}","#{pet_pic}","#{pet_description}"]}
             #descriptions.keys
-                puts descriptions.select{|k,v| k == "true"}
-                i+=1
-            end
+            descriptions.select{|k,v| k == "true"}
+            i+=1
+        end
     end
 end
 
 #TESTING  
-a = Pets.new("dog")
-puts a.get_pet_files
+#a = Pets.new("dog")
+#puts a.get_pet_files
 
 
